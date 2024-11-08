f9e29d5fd08c17767377584b6454004d32375a11
Fatima El mouhine <91740491+fatima-elmouhine@users.noreply.github.com>
2024-11-08 10:12:59 +0100

    Update app.js

--- back/app.js
+++ back/app.js
@@ -33,6 +33,8 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get(‘*’, (req, res) => { res.sendFile(path.join(__dirname, ‘public’, ‘index.html’)); });
+
 app.use(function(req, res, next) {
   next(createError(404));
 });
